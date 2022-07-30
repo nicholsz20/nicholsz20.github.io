faf9442a6b6ec92d5c617a3695dfee80619cd893
Patrick-Dev <Patrickattabaah16@gmail.com>
2022-07-30 11:50:48 -0400

    added server.js

--- src/server.js
+++ src/server.js
@@ -1,47 +1,79 @@
-const express = require("express");
-const router = express.Router();
-const cors = require("cors");
-const nodemailer = require("nodemailer");
+import axios from 'axios' 
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
 
-const contactEmail = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: "todd.carter54@gmail.com",
-        pass: "Denise9191",
+
+const backendURI = "backend url link here"
+const getLocalAccessToken = () => {
+    const accessToken = localStorage.getItem('access')
+    return accessToken 
+}
+
+const getLocalRefreshToken = () => {
+    const refreshToken = localStorage.getItem('refresh') 
+    return refreshToken 
+}
+
+
+
+const auth_instance = axios.create({
+    baseURL: backendURI,
+    headers: {
+        'Content-Type': 'application/json',
+    }, 
+})
+
+
+
+
+auth_instance.interceptors.request.use( (config) => {
+    const token = getLocalAccessToken() 
+    if(token){
+        config.headers['Authorization'] = "Bearer " + token 
     }
-});
+    return config 
+}, (error) => {
+    return Promise.reject(error)
+})
 
-contactEmail.verify((error) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Ready to Send")
-    }
-});
 
-router.post("/contact", (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message;
-    const mail = {
-        from: name,
-        to: "@gmail.com",
-        subject: "Contact Form Submisson",
-        html: `<p>Name: ${name}</p>
-               <p>Email: ${email}</p>
-               <p>Messgae: ${message} </p>`,
-    }
-    contactEmail.sendMail(mail, (error) => {
-        if (error) {
-            res.json({ status: "ERROR" });
-        } else {
-            res.json({ status: "Message Sent" });
+let refreshing_token = null 
+auth_instance.interceptors.response.use( (response) => {
+    return response 
+}, async (error) => {
+    const originalConfig = error.config 
+    if(error.response){
+        if(error.response.status == 401 && !originalConfig._retry){
+            originalConfig._retry = true 
+            try{
+                refreshing_token = refreshing_token ? refreshing_token : refreshToken() 
+                const rs = await refreshing_token
+                if(rs.data.access){
+                    const { access } = rs.data 
+                    localStorage.setItem('access', access)
+                    auth_instance.defaults.headers.common['Authorization'] = "Bearer " + access 
+                }
+               
+                return auth_instance(originalConfig) 
+            }catch(_error){
+                if(_error.response && _error.response.data){
+                    return Promise.reject(_error.response.data)
+                }
+                return Promise.reject(_error)
+            }
         }
-    });
-});
+
+        if(error.response.status == 403 && error.response.data){
+            return Promise.reject(error.response.data) 
+        }
+    }
+
+    return Promise.reject(error)
+
+
+})
+
+const refreshToken = async () => {
+    return await auth_instance.post('link to token', {
+        refresh: getLocalRefreshToken(), 
+    })
+}
