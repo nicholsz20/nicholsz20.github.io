13ffbb43ce14d98f9eff9686af04350d27c14b25
Patrick-Dev <Patrickattabaah16@gmail.com>
2022-07-30 12:44:02 -0400

    updated aboutme.js #2

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -29,6 +29,7 @@ const About = () => {
           display="flex">
 
           <CardMedia 
+            sx={{ overflow: 'hidden', objectFit: 'cover', width: '100%', height: '100%'}}
             src={toddprofile}
             component="img"/>
 
