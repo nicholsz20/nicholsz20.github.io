2431299620e8056a44929a8efd9194e08a7d7939
nicholsz20 <79768423+nicholsz20@users.noreply.github.com>
2022-08-07 19:50:54 -0400

    Added in services

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,6 +1,7 @@
 import { Carousel } from 'react-carousel-minimal';
 import { Box, Stack } from '@mui/material'
 import Typography from '@mui/material/Typography'
+import Services from './Services'
 
 
 
@@ -11,83 +12,77 @@ import skate_park_biker from '../portfolio/skate_park_biker.JPG'
 import train_tracks from '../portfolio/train_tracks.JPG'
 
 function Welcome() {
- const data = [
-    {
+  const data = [
+     {
       image: roeVsWade,
       caption: "Roe vs Wade"
-    },
-    {
+      },
+      {
       image: artSculpture,
       caption: "Band"
-    },
-    {
+      },
+      {
       image: train_tracks,
       caption: "Train"
-    },
-    {
+      },
+      {
       image: downtown_raleigh_skyline,
       caption: "Raleigh Downtown"
-    },
-    {
+      },
+      {
       image: skate_park_biker,
       caption: "Skate Park"
-    }
-  ];
-
-  const captionStyle = {
-    fontSize: '2em',
-    fontWeight: 'bold',
-  }
-  const slideNumberStyle = {
-    fontSize: '20px',
-    fontWeight: 'bold',
-  }
-  return (
-    <Box 
-      maxWidth={"80%"}
-      justifyContent={"center"}
-      alignItems={"center"}>
-
-      <Typography
-        variant="h4"
-        className="Welcome Title" 
-        fontFamily="MainFont4"
-        style={{ textAlign: "center" }}
-      >
-          Welcome To My Site
-      </Typography>
-        <div style={{
-          padding: "0 20px"
-        }}>
-          <Carousel
-            data={data}
-            time={2000}
-            width="500px"
-            height="500px"
-            captionStyle={captionStyle}
-            radius="10px"
-            slideNumber={true}
-            fontFamily="MainFont4"
-            slideNumberStyle={slideNumberStyle}
-            captionPosition="bottom"
-            automatic={true}
-            dots={true}
-            pauseIconColor="white"
-            pauseIconSize="40px"
-            slideBackgroundColor="darkgrey"
-            slideImageFit="cover"
-            thumbnails={true}
-            thumbnailWidth="100px"
-            style={{
-              textAlign: "center",
-              width: "auto",
-              height: "500px",
-              margin: "auto",
-            }}
-          />
-        </div>
-    </Box>
-  );
-}
+      }
+      ];
+ 
+   const captionStyle = {
+     fontSize: '2em',
+     fontWeight: 'bold',
+   }
+   const slideNumberStyle = {
+     fontSize: '20px',
+     fontWeight: 'bold',
+   }
+   return (
+     <div className="App">
+       <div style={{ textAlign: "center" }}>
+         <h2>React Carousel Minimal</h2>
+         <p>Easy to use, responsive and customizable carousel component for React Projects.</p>
+         <div style={{
+           padding: "0 20px"
+         }}>
+           <Carousel
+             data={data}
+             time={2000}
+             width="850px"
+             height="500px"
+             captionStyle={captionStyle}
+             radius="10px"
+             slideNumber={true}
+             slideNumberStyle={slideNumberStyle}
+             captionPosition="bottom"
+             automatic={true}
+             dots={true}
+             pauseIconColor="white"
+             pauseIconSize="40px"
+             slideBackgroundColor="darkgrey"
+             slideImageFit="cover"
+             thumbnails={true}
+             thumbnailWidth="100px"
+             style={{
+               textAlign: "center",
+               maxWidth: "850px",
+               maxHeight: "500px",
+               margin: "40px auto",
+             }}
+           />
+         </div>
+         <br />
+         <br />
+        <Services />
+       </div>
+     </div>
+   );
+ }
 
 export default Welcome;
